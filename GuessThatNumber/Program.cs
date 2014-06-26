ff4804db4d05c2b0a942811dd8ee8e4048e20287
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 20:36:15 -0600

    first upload

---
+++ GuessThatNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // can you guess the number?
+
+
+
+
+
+
+            //to keep console open
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
